5e97ae7d2eb03026da491cde3a160d3c00b2516a
Jose Jesse Renteria <joserenteria@hotmail.com>
2022-06-18 10:39:24 -0400

    Searcher() implemented with tests

---
+++ BinarySearchTree.Tests/BtSearcherTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+
+namespace BinarySearchTree.Tests;
+
+[TestFixture]
+public class BtSearcherTests
+{
+    private BinaryTree _binaryTree;
+    private Node _rootNode;
+
+    [SetUp]
+    public void Setup()
+    {
+        _binaryTree = new BinaryTree(8);
+        _rootNode = _binaryTree.Root;
+        Assert.IsTrue(BtValueInserter.InsertNode(3, _rootNode));
+        Assert.IsTrue(BtValueInserter.InsertNode(10, _rootNode));
+        Assert.IsTrue(BtValueInserter.InsertNode(1, _rootNode));
+        Assert.IsTrue(BtValueInserter.InsertNode(6, _rootNode));
+        Assert.IsTrue(BtValueInserter.InsertNode(4, _rootNode));
+    }
+
+    [Test]
+    public void RootIsFound()
+    {
+        Assert.AreEqual(new Node(8).Value, BtSearcher.GetValue(8, _rootNode).Value);
+    }
+
+    [Test]
+    public void FirstLevelGreaterThanRootFound()
+    {
+        Assert.AreEqual(new Node(10).Value, BtSearcher.GetValue(10, _rootNode).Value);
+    }
+
+    [Test]
+    public void DescendantLessThanOthersFound()
+    {
+        Assert.AreEqual(new Node(1).Value, BtSearcher.GetValue(1, _rootNode).Value);
+    }
+
+    [Test]
+    public void LastDescendantWithLessAndGreaterParentsFound()
+    {
+        Assert.AreEqual(new Node(4).Value, BtSearcher.GetValue(4, _rootNode).Value);
+    }
+
+    [TestCase(12)]
+    [TestCase(5)]
+    [TestCase(30)]
+    [TestCase(0)]
+    public void NotFoundReturnsNull(int value)
+    {
+        Assert.IsNull(BtSearcher.GetValue(value, _rootNode));
+    }
+}
---
+++ BinarySearchTree/BtSearcher.cs
@@ -0,0 +1,30 @@
+﻿namespace BinarySearchTree;
+
+public static class BtSearcher
+{
+    public static Node GetValue(int value, Node node)
+    {
+        if (node == null)
+            return null;
+
+        while (true)
+        {
+            if (node.Value == value)
+                return node;
+
+            if (value < node.Value && node.LeftNode != null)
+            {
+                node = node.LeftNode;
+                continue;
+            }
+
+            if (value > node.Value && node.RightNode != null)
+            {
+                node = node.RightNode;
+                continue;
+            }
+
+            return null;
+        }
+    }
+}
